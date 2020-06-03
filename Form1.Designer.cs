dc95be248f98a9ce0ca8f100928fbf9c08550f8f
ten toi la long <longpvth1907014@fpt.edu.vn>
2020-06-03 22:56:58 +0700

    Assigment

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace QLKSan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxTinhTrang = new System.Windows.Forms.ComboBox();
+            this.txtIdRoom = new System.Windows.Forms.TextBox();
+            this.txtNameRoom = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtNameUser = new System.Windows.Forms.TextBox();
+            this.cbxLoaiPhong = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.tblRoom = new System.Windows.Forms.DataGridView();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.clid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clMaPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClTenPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tblRoom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Phòng :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Phòng :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giá :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tên người đặt :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 4;
+            // 
+            // cbxTinhTrang
+            // 
+            this.cbxTinhTrang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxTinhTrang.FormattingEnabled = true;
+            this.cbxTinhTrang.Items.AddRange(new object[] {
+            "Còn",
+            "Hết"});
+            this.cbxTinhTrang.Location = new System.Drawing.Point(167, 206);
+            this.cbxTinhTrang.Name = "cbxTinhTrang";
+            this.cbxTinhTrang.Size = new System.Drawing.Size(108, 24);
+            this.cbxTinhTrang.TabIndex = 5;
+            this.cbxTinhTrang.Text = "Tình Trạng";
+            // 
+            // txtIdRoom
+            // 
+            this.txtIdRoom.Location = new System.Drawing.Point(167, 24);
+            this.txtIdRoom.Name = "txtIdRoom";
+            this.txtIdRoom.Size = new System.Drawing.Size(223, 20);
+            this.txtIdRoom.TabIndex = 6;
+            // 
+            // txtNameRoom
+            // 
+            this.txtNameRoom.Location = new System.Drawing.Point(167, 69);
+            this.txtNameRoom.Name = "txtNameRoom";
+            this.txtNameRoom.Size = new System.Drawing.Size(223, 20);
+            this.txtNameRoom.TabIndex = 7;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(167, 114);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(223, 20);
+            this.txtPrice.TabIndex = 8;
+            // 
+            // txtNameUser
+            // 
+            this.txtNameUser.Location = new System.Drawing.Point(167, 156);
+            this.txtNameUser.Name = "txtNameUser";
+            this.txtNameUser.Size = new System.Drawing.Size(223, 20);
+            this.txtNameUser.TabIndex = 9;
+            // 
+            // cbxLoaiPhong
+            // 
+            this.cbxLoaiPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxLoaiPhong.FormattingEnabled = true;
+            this.cbxLoaiPhong.Items.AddRange(new object[] {
+            "VIP",
+            "Thường"});
+            this.cbxLoaiPhong.Location = new System.Drawing.Point(282, 206);
+            this.cbxLoaiPhong.Name = "cbxLoaiPhong";
+            this.cbxLoaiPhong.Size = new System.Drawing.Size(108, 24);
+            this.cbxLoaiPhong.TabIndex = 10;
+            this.cbxLoaiPhong.Text = "Loại Phòng";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(44, 253);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 35);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(222, 253);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 35);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(125, 253);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 35);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // tblRoom
+            // 
+            this.tblRoom.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblRoom.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clid,
+            this.clMaPhong,
+            this.ClTenPhong,
+            this.clGia});
+            this.tblRoom.Location = new System.Drawing.Point(12, 294);
+            this.tblRoom.Name = "tblRoom";
+            this.tblRoom.Size = new System.Drawing.Size(646, 256);
+            this.tblRoom.TabIndex = 14;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(315, 253);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 35);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(421, 253);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(115, 34);
+            this.btnSearch.TabIndex = 16;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // clid
+            // 
+            this.clid.DataPropertyName = "id";
+            this.clid.HeaderText = "ID";
+            this.clid.Name = "clid";
+            // 
+            // clMaPhong
+            // 
+            this.clMaPhong.DataPropertyName = "MaPhong";
+            this.clMaPhong.HeaderText = "Mã Phòng";
+            this.clMaPhong.Name = "clMaPhong";
+            // 
+            // ClTenPhong
+            // 
+            this.ClTenPhong.DataPropertyName = "TenPhong";
+            this.ClTenPhong.HeaderText = "Tên Phòng";
+            this.ClTenPhong.Name = "ClTenPhong";
+            // 
+            // clGia
+            // 
+            this.clGia.DataPropertyName = "Gia";
+            this.clGia.HeaderText = "Giá";
+            this.clGia.Name = "clGia";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 562);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.tblRoom);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbxLoaiPhong);
+            this.Controls.Add(this.txtNameUser);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtNameRoom);
+            this.Controls.Add(this.txtIdRoom);
+            this.Controls.Add(this.cbxTinhTrang);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "Quản Lí Khách Sạn";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblRoom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbxTinhTrang;
+        private System.Windows.Forms.TextBox txtIdRoom;
+        private System.Windows.Forms.TextBox txtNameRoom;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtNameUser;
+        private System.Windows.Forms.ComboBox cbxLoaiPhong;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView tblRoom;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clMaPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClTenPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clGia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.ComponentModel.Design;
+using System.Configuration;
+
+namespace QLKSan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();        
+        }
+        SqlConnection con;
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtIdRoom.Text = "";
+            txtNameRoom.Text = "";
+            txtPrice.Text = "";
+            txtNameUser.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["QLKS"].ConnectionString.ToString();
+            con = new SqlConnection(conString);
+            con.Open();
+            Display();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            con.Close();
+        }
+        public void Display()
+        {
+            string sqlSELECT = "SELECT id,MaPhong,TenPhong,Gia FROM KSan ORDER BY id ASC ";
+            SqlCommand cmd = new SqlCommand(sqlSELECT, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            tblRoom.DataSource = dt;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string SqlINSERT = "INSERT INTO KSan VALUES (@MaPhong, @TenPhong,@Gia,@TenNguoiDat,@LoaiPhong,@TinhTrang)";
+            SqlCommand cmd = new SqlCommand(SqlINSERT,con);
+            cmd.Parameters.AddWithValue("MaPhong", txtIdRoom.Text);
+            cmd.Parameters.AddWithValue("TenPhong", txtNameRoom.Text);
+            cmd.Parameters.AddWithValue("Gia", txtPrice.Text);
+            cmd.Parameters.AddWithValue("TenNguoiDat", txtNameUser.Text);
+            cmd.Parameters.AddWithValue("LoaiPhong", cbxLoaiPhong.Text);
+            cmd.Parameters.AddWithValue("TinhTrang", cbxTinhTrang.Text);
+            cmd.ExecuteNonQuery();
+            Display();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string SqlUpdate = "UPDATE KSan SET TenPhong = @TenPhong , Gia = @Gia, TenNguoiDat = @TenNguoiDat,LoaiPhong = @LoaiPhong,TinhTrang = @TinhTrang WHERE MaPhong = @MaPhong ";
+            SqlCommand cmd = new SqlCommand(SqlUpdate, con);
+            cmd.Parameters.AddWithValue("MaPhong", txtIdRoom.Text);
+            cmd.Parameters.AddWithValue("TenPhong", txtNameRoom.Text);
+            cmd.Parameters.AddWithValue("Gia", txtPrice.Text);
+            cmd.Parameters.AddWithValue("TenNguoiDat", txtNameUser.Text);
+            cmd.Parameters.AddWithValue("LoaiPhong", cbxLoaiPhong.Text);
+            cmd.Parameters.AddWithValue("TinhTrang", cbxTinhTrang.Text);
+            cmd.ExecuteNonQuery();
+            Display();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string SqlDelete = "DELETE FROM KSan WHERE MaPhong = @MaPhong";
+            SqlCommand cmd = new SqlCommand(SqlDelete, con);
+            cmd.Parameters.AddWithValue("MaPhong", txtIdRoom.Text);
+            cmd.Parameters.AddWithValue("TenPhong", txtNameRoom.Text);
+            cmd.Parameters.AddWithValue("Gia", txtPrice.Text);
+            cmd.Parameters.AddWithValue("TenNguoiDat", txtNameUser.Text);
+            cmd.Parameters.AddWithValue("LoaiPhong", cbxLoaiPhong.Text);
+            cmd.Parameters.AddWithValue("TinhTrang", cbxTinhTrang.Text);
+            cmd.ExecuteNonQuery();
+            Display();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string SqlSearch = "SELECT * FROM KSan WHERE MaPhong = @MaPhong";
+            SqlCommand cmd = new SqlCommand(SqlSearch, con);
+            cmd.Parameters.AddWithValue("MaPhong", txtIdRoom.Text);
+            cmd.Parameters.AddWithValue("TenPhong", txtNameRoom.Text);
+            cmd.Parameters.AddWithValue("Gia", txtPrice.Text);
+            cmd.Parameters.AddWithValue("TenNguoiDat", txtNameUser.Text);
+            cmd.Parameters.AddWithValue("LoaiPhong", cbxLoaiPhong.Text);
+            cmd.Parameters.AddWithValue("TinhTrang", cbxTinhTrang.Text);
+            cmd.ExecuteNonQuery();
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            tblRoom.DataSource = dt;
+        }
+    }
+}
